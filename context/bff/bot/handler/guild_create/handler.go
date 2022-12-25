f41df650a3d3df29620f801c65c02b5afcc1b622
totsuka <totsuka@totsukanoMacBook-Air.local>
2022-12-25 21:03:32 +0900

    新規サーバー導入時に通知を追加(fix #12)

--- context/bff/bot/handler/guild_create/handler.go
+++ context/bff/bot/handler/guild_create/handler.go
@@ -7,6 +7,5 @@ import (
 // botが追加された時のハンドラーです
 func Handler(s *discordgo.Session, m *discordgo.GuildCreate) {
 	AddCommandHandler(s, m)
-
-	// TODO: 新規サーバーを通知します
+	NoticeHandler(s, m)
 }
---
+++ context/bff/bot/handler/guild_create/notice.go
@@ -0,0 +1,30 @@
+package guild_create
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"github.com/techstart35/auto-reply-bot/context/discord/expose/conf"
+	"github.com/techstart35/auto-reply-bot/context/discord/expose/message_send"
+	"github.com/techstart35/auto-reply-bot/context/shared/errors"
+)
+
+const ContentTmpl = `
+サーバー名: **%s**
+ID: %s
+`
+
+// 新規サーバーに導入されたことを通知します
+func NoticeHandler(s *discordgo.Session, m *discordgo.GuildCreate) {
+	req := message_send.SendMessageEmbedReq{
+		ChannelID: conf.ErrMsgChannelID,
+		Title:     "新規サーバーに追加されました",
+		Content:   fmt.Sprintf(ContentTmpl, m.Guild.Name, m.Guild.ID),
+		Color:     conf.ColorGreen,
+	}
+
+	if err := message_send.SendMessageEmbed(s, req); err != nil {
+		message_send.SendErrMsg(
+			s, errors.NewError("新規サーバー導入時の通知を送信できません", err),
+		)
+	}
+}
