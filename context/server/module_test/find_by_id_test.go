6eef925517220de031cab714ecf714631096aae1
totsuka <totsuka@totsukanoMacBook-Air.local>
2023-01-03 15:26:21 +0900

    サーバーコンテキストのテストを追加

--- context/server/module_test/find_by_id_test.go
+++ context/server/module_test/find_by_id_test.go
@@ -95,4 +95,51 @@ func TestFindByID(t *testing.T) {
 			t.Fatal("エラーが返されなかった")
 		}
 	})
+
+	t.Run("IsAllMatch=trueの時にall-containのレスポンスが返される", func(t *testing.T) {
+		ctx, teardown := setup(t)
+		defer teardown()
+
+		// 外部APIをモックします
+		MockExternal(
+			TestNow,
+		)
+
+		// モックデータを登録します
+		{
+			blockMock := map[string]interface{}{}
+			gen.Gen(blockMock, []string{"keyword"}, []map[string]interface{}{
+				{"value": "k1"},
+				{"value": "k2"},
+			})
+			gen.Gen(blockMock, []string{"reply"}, []map[string]interface{}{
+				{"value": "r1"},
+				{"value": "r2"},
+			})
+			gen.Gen(blockMock, []string{"is_all_match"}, true)
+			gen.Gen(blockMock, []string{"is_random"}, true)
+			gen.Gen(blockMock, []string{"is_embed"}, true)
+
+			mock := ServerInitialMock(TestID)
+			gen.Gen(mock, []string{"admin_role_id", "value"}, TestAdminRoleID)
+			gen.Gen(mock, []string{"block"}, []map[string]interface{}{blockMock})
+			RegisterServer(t, mock)
+		}
+
+		// テスト対象のAPIをコールします
+		res, err := v1.FindByID(ctx, TestID)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(res.Block) != 1 {
+			t.Fatal("期待した値と一致しません")
+		}
+
+		blockRes := res.Block[0]
+
+		if blockRes.MatchCondition != block.MatchConditionAllContain {
+			t.Fatal("期待した値と一致しません")
+		}
+	})
 }
--- context/server/module_test/update_config_test.go
+++ context/server/module_test/update_config_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/bwmarrin/discordgo"
 	"github.com/techstart35/auto-reply-bot/context/server/domain/model/server/block"
 	"github.com/techstart35/auto-reply-bot/context/server/expose/api/v1"
+	"github.com/techstart35/auto-reply-bot/context/shared/map/gen"
 	"reflect"
 	"testing"
 )
@@ -63,6 +64,63 @@ func TestUpdateConfig(t *testing.T) {
 		}
 
 		if !reflect.DeepEqual(res.Block, []v1.BlockRes{expectBlockRes}) {
+			t.Fatal("期待した値と一致しません")
+		}
+	})
+
+	t.Run("IsAllMatch=trueが入っている状態で、MatchConditionに変更できる", func(t *testing.T) {
+		ctx, teardown := setup(t)
+		defer teardown()
+
+		// 外部APIをモックします
+		MockExternal(
+			TestNow,
+		)
+
+		// モックデータを登録します
+		{
+			blockMock := map[string]interface{}{}
+			gen.Gen(blockMock, []string{"keyword"}, []map[string]interface{}{
+				{"value": "k1"},
+				{"value": "k2"},
+			})
+			gen.Gen(blockMock, []string{"reply"}, []map[string]interface{}{
+				{"value": "r1"},
+				{"value": "r2"},
+			})
+			gen.Gen(blockMock, []string{"is_all_match"}, true)
+			gen.Gen(blockMock, []string{"is_random"}, true)
+			gen.Gen(blockMock, []string{"is_embed"}, true)
+
+			mock := ServerInitialMock(TestID)
+			gen.Gen(mock, []string{"admin_role_id", "value"}, TestAdminRoleID)
+			gen.Gen(mock, []string{"block"}, []map[string]interface{}{blockMock})
+			RegisterServer(t, mock)
+		}
+
+		// テスト対象のAPIをコールします
+		// モックデータと同じ値を設定します
+		blockReq1 := v1.BlockReq{
+			Name:           "foo",
+			Keyword:        []string{"k1", "k2"},
+			Reply:          []string{"r1", "r2"},
+			MatchCondition: block.MatchConditionAllContain,
+			IsRandom:       true,
+			IsEmbed:        true,
+		}
+
+		res, err := v1.UpdateConfig(
+			&discordgo.Session{},
+			ctx,
+			TestID,
+			TestAdminRoleID,
+			[]v1.BlockReq{blockReq1},
+		)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if res.Block[0].MatchCondition != block.MatchConditionAllContain {
 			t.Fatal("期待した値と一致しません")
 		}
 	})
