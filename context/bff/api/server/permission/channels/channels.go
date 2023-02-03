40d6864ba10215d1ab38ad8e7e97bcc6f62a2c27
totsuka <totsuka@totsukanoMacBook-Air.local>
2023-02-03 10:44:51 +0900

    権限一覧を修正

--- context/bff/api/server/permission/channels/channels.go
+++ context/bff/api/server/permission/channels/channels.go
@@ -3,6 +3,7 @@ package channels
 import (
 	"github.com/bwmarrin/discordgo"
 	"github.com/gin-gonic/gin"
+	"github.com/techstart35/auto-reply-bot/context/bff/shared"
 	"github.com/techstart35/auto-reply-bot/context/discord/expose/info/guild"
 	"github.com/techstart35/auto-reply-bot/context/discord/expose/initiate"
 	"github.com/techstart35/auto-reply-bot/context/discord/expose/message_send"
@@ -77,14 +78,21 @@ func getServerPermissionChannels(c *gin.Context) {
 		return
 	}
 
+	ctx, _, err := shared.CreateDBTx()
+	if err != nil {
+		message_send.SendErrMsg(session, errors.NewError("DBのTxを作成できません", err), "none")
+		c.JSON(http.StatusInternalServerError, "サーバーエラーが発生しました")
+		return
+	}
+
 	id := c.Query("id")
-	//token := c.GetHeader("Token")
+	token := c.GetHeader("Token")
 
 	// リクエストを検証します
-	//if id == "" || token == "" {
-	//	c.JSON(http.StatusBadRequest, "リクエストが不正です")
-	//	return
-	//}
+	if id == "" || token == "" {
+		c.JSON(http.StatusBadRequest, "リクエストが不正です")
+		return
+	}
 
 	// クエリパラメータに指定されたサーバーです
 	guildName, err := guild.GetGuildName(session, id)
@@ -94,16 +102,16 @@ func getServerPermissionChannels(c *gin.Context) {
 	}
 
 	// 認証されているユーザーかを検証します
-	//ok, err := shared.IsAuthorizedUser(ctx, session, id, token)
-	//if err != nil {
-	//	message_send.SendErrMsg(session, errors.NewError("認証されているかの確認に失敗しました", err), guildName)
-	//	c.JSON(http.StatusUnauthorized, "認証されていません")
-	//	return
-	//}
-	//if !ok {
-	//	c.JSON(http.StatusUnauthorized, "認証されていません")
-	//	return
-	//}
+	ok, err := shared.IsAuthorizedUser(ctx, session, id, token)
+	if err != nil {
+		message_send.SendErrMsg(session, errors.NewError("認証されているかの確認に失敗しました", err), guildName)
+		c.JSON(http.StatusUnauthorized, "認証されていません")
+		return
+	}
+	if !ok {
+		c.JSON(http.StatusUnauthorized, "認証されていません")
+		return
+	}
 
 	res, err := getAllPermission(session, id)
 	if err != nil {
